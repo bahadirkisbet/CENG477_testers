326273bf1eaf908a2c5ed2bf5cb5d6ddd33c0898
= <=>
2021-11-21 14:03:48 +0300

    Added tester of first homework

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# CENG477_testers
+# CENG477 Testers
+
+## HW1
+
+The program iterates over the pixels and change the color of the pixels that are different in source image and target image.
+
+```
+python3 hw1_tester.py source_image target_image comparison_output epsilon color 
+```
+
+where   `source_image` is the sample scene, ( Example: `../sample_scenes/outputs/bfc_on/monkey.ppm`) <br />
+        `target_image` is the result of your code, ( Example `outputs/monkey.ppm`) <br />
+        `comparison_output` is the result image that has been colored by the `color` given by the user <br />
+        `epsilon` is the acceptable int range for a pixel to be considered the same ( Exmaple: (255,255,255) == (255,254,254) ) <br />
+        `color` is the color preferred by the user in order to change the color of different pixels in the source and target files <br />
+
+
+Color Options = [white, yellow, cyan, green, magenta, red, blue, black] or specified by the user with three integer.
+
+### Example Run
+
+```
+python3 tester.py test/simple_reflectance.ppm test/bfc_off_simple_reflectance.ppm comparison_image.ppm 5 cyan
+```
+
+### Example Output
+
+![Aata](./img/comparison_image.png)
---
+++ tester.py
@@ -0,0 +1,87 @@
+import sys
+from enum import Enum
+
+class COLORS(Enum):
+    white       = 0
+    yellow      = 1
+    cyan        = 2
+    green       = 3
+    magenta     = 4
+    red         = 5
+    blue        = 6
+    black       = 7
+
+
+BAR_COLOR = [
+    [ 255, 255, 255 ],  # 100% White
+    [ 255, 255,   0 ],  # Yellow
+    [   0, 255, 255 ],  # Cyan
+    [   0, 255,   0 ],  # Green
+    [ 255,   0, 255 ],  # Magenta
+    [ 255,   0,   0 ],  # Red
+    [   0,   0, 255 ],  # Blue
+    [   0,   0,   0 ],  # Black
+]
+
+if __name__ == "__main__":
+    args        = sys.argv
+    source      = args[1]
+    target      = args[2]
+    color       = None
+    comp_path   = args[3]
+    epsilon     = int(args[4])
+
+    if len(args) > 6:
+        color = [int(args[5]), int(args[6]), int(args[7])]
+        assert color[0] <= 255 and color[1] <= 255 and color[2] <= 255, "The pixel is out of range"
+    else:
+        color = eval("COLORS." + args[5]).value
+        color = BAR_COLOR[color]
+    
+    source_d = None
+    with open(source, "r") as f:
+        source_d = f.read()
+
+    target_d = None
+    with open(target, "r") as f:
+        target_d = f.read()
+
+    source_d = source_d.split("\n")
+    target_d = target_d.split("\n")
+    
+
+    height, width   = source_d[1].split()
+    height          = int(height)
+    width           = int(width)
+    pixel_limit     = int(source_d[2])
+    result_image    = source_d[:3]
+    source_d        = source_d[3:]
+    target_d        = target_d[3:]
+    succesfull      = 0
+    error           = 0
+
+    for i in range(width):
+        src_row     = list(map(int,source_d[i].split()))
+        target_row  = list(map(int,target_d[i].split()))
+        result_row  = []
+        for j in range(height):
+            if  abs(src_row[j * 3]      - target_row[j * 3])        < epsilon   and \
+                abs(src_row[j * 3 + 1]  - target_row[j * 3 + 1])    < epsilon   and \
+                abs(src_row[j * 3 + 2]  - target_row[j * 3 + 2])    < epsilon: # the pixel is the same.
+                succesfull += 1
+                result_row += src_row[j*3 : j*3 + 3]
+                
+            else: # the pixel is wrong
+                result_row += color
+                error += 1
+        result_image.append(
+            " ".join(list(map(str, result_row))) # convert every element into a string and join them.
+        )
+
+    print(f"Total pixel num -> {succesfull + error}")
+    print(f"Total sucessfull pixel -> {succesfull}")
+    print(f"Total erroroneus pixel -> {error}")
+    print(f"The percentage of erroroneus pixel {error / (succesfull + error)}")
+    result = "\n".join(result_image)
+    with open(comp_path, "w") as f:
+        f.write(result)
